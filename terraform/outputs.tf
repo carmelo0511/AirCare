0117818efc79702baa7384158bbcd86d5412eff1
Bryan Nakache <nakachebryan@gmail.com>
2025-06-14 13:52:12 -0400

    Fix lambda deployment step

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -13,3 +13,7 @@ output "api_gateway_url" {
 output "dynamodb_table_name" {
   value = aws_dynamodb_table.history_table.name
 }
+
+output "lambda_role_arn" {
+  value = aws_iam_role.lambda_exec.arn
+}
