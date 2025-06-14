897c4d70e6c2f4c171fd693fc94b7f47e7b2c3bf
Bryan Nakache <nakachebryan@gmail.com>
2025-06-13 22:48:51 -0400

    Fix terraform backend variables and close deployment block

--- terraform/main.tf
+++ terraform/main.tf
@@ -130,6 +130,8 @@ resource "aws_api_gateway_deployment" "aircare_deployment" {
     aws_api_gateway_method.history
   ]
 
+}
+
 resource "aws_api_gateway_stage" "prod" {
   stage_name    = var.stage_name
   rest_api_id   = aws_api_gateway_rest_api.aircare_api.id
